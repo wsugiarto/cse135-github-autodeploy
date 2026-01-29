9e28bdd948b7b06606b61597f780a1d6dd3f2b8f
Wilson Sugiarto <sugiarto.wilson@gmail.com>
2026-01-28 18:02:25 -0800

    sessions

--- wsugiarto.site/public_html/hw2/perl/perl-sessions-1.pl
+++ wsugiarto.site/public_html/hw2/perl/perl-sessions-1.pl
@@ -30,9 +30,9 @@ if ($name){
 	print "<p><b>Name:</b> You do not have a name set</p>";
 }
 print "<br/><br/>";
-print "<a href=\"/cgi-bin/perl-sessions-2.pl\">Session Page 2</a><br/>";
+print "<a href=\"perl-sessions-2.pl\">Session Page 2</a><br/>";
 print "<a href=\"/perl-cgiform.html\">Perl CGI Form</a><br />";
-print "<form style=\"margin-top:30px\" action=\"/cgi-bin/perl-destroy-session.pl\" method=\"get\">";
+print "<form style=\"margin-top:30px\" action=\"perl-destroy-session.pl\" method=\"get\">";
 print "<button type=\"submit\">Destroy Session</button>";
 print "</form>";
 
--- wsugiarto.site/public_html/hw2/perl/perl-sessions-2.pl
+++ wsugiarto.site/public_html/hw2/perl/perl-sessions-2.pl
@@ -33,9 +33,9 @@ if ($name){
 	print "<p><b>Name:</b> You do not have a name set</p>";
 }
 print "<br/><br/>";
-print "<a href=\"/cgi-bin/perl-sessions-1.pl\">Session Page 1</a><br/>";
-print "<a href=\"/perl-cgiform.html\">Perl CGI Form</a><br />";
-print "<form style=\"margin-top:30px\" action=\"/cgi-bin/perl-destroy-session.pl\" method=\"get\">";
+print "<a href=\"perl-sessions-1.pl\">Session Page 1</a><br/>";
+print "<a href=\"perl-cgiform.html\">Perl CGI Form</a><br />";
+print "<form style=\"margin-top:30px\" action=\"perl-destroy-session.pl\" method=\"get\">";
 print "<button type=\"submit\">Destroy Session</button>";
 print "</form>";
 
